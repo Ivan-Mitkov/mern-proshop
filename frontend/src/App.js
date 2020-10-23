0a95032a345c909dcbff4e71e23754849e64035c
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-23 14:11:45 +0300

    add bootstrap header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import Layout from "./components/layout";
 function App() {
   return (
     <div className="App">
-     Pro com
+      <Layout>
+        <main className="py-3">
+          <Container>
+            <h1>Pro Com</h1>
+          </Container>
+        </main>
+      </Layout>
     </div>
   );
 }
