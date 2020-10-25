6a29eff00148a2f2e3dff2c24a1a40b4cedeb130
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-25 16:41:39 +0200

    spinner and message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,22 +3,26 @@ import { Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import Product from "../components/product";
 import { listProducts } from "../actions/productActions";
-
+import Spinner from "../components/loader";
+import Message from "../components/message";
 const HomeScreen = () => {
   const dispatch = useDispatch();
+
   const productList = useSelector((state) => state.productList);
   const { products, loading, error } = productList;
+
   useEffect(() => {
     dispatch(listProducts());
     console.log("Use effect", products);
   }, [dispatch]);
+
   return (
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h3>Loading...</h3>
+        <Spinner />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products &&
