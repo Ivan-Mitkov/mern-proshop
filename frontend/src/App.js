65ed215516a1dece2f003f588ea83d75393c84d0
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-28 14:08:40 +0200

    user login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ import Layout from "./components/layout";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 function App() {
   return (
     <Router>
       <Layout>
         <main className="py-3">
           <Container>
-            <Route path="/" component={HomeScreen} exact />
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/" component={HomeScreen} exact />
           </Container>
         </main>
       </Layout>
