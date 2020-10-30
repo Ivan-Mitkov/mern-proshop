34be2522cf653c21fb44b3409c4f696c8402fef7
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-30 14:34:50 +0200

    Place order screen

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -2,11 +2,11 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
-  CART_SAVE_PAYMENT_METHOD
+  CART_SAVE_PAYMENT_METHOD,
 } from "../constants/cartConsts";
 
 export const cartReducer = (
-  state = { cartItems: [], shippingAddress: {} },
+  state = { cartItems: [], shippingAddress: {}, paymentMethod: "" },
   action
 ) => {
   const { type, payload } = action;
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,14 +1,141 @@
-import React from 'react'
-import { Form, Button } from "react-bootstrap";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import FormContainer from "../components/formcontainer";
-import CheckoutSteps from "../components/checkoutSteps"
+import Message from "../components/message";
+import CheckoutSteps from "../components/checkoutSteps";
 const PlaceOrderScreen = () => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  //Calculate Prices
+  cart.itemsPrice = Number(
+    cart.cartItems.reduce((a, b) => a + b.price * b.qty, 0).toFixed(2)
+  );
+  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 10;
+  const taxRate = 0.2;
+  cart.taxPrice = Number(
+    ((cart.itemsPrice + cart.shippingPrice) * taxRate).toFixed(2)
+  );
+  cart.totalPrice = Number(
+    (cart.shippingPrice + cart.taxPrice + cart.itemsPrice).toFixed(2)
+  );
+  //handlers
+  const handlePlaceOrder = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}
+              </p>
+              <p>
+                <strong>City: </strong>
+                {cart.shippingAddress.city}
+              </p>
+              <p>
+                <strong>Country: </strong>
+                {cart.shippingAddress.country}
+              </p>
+              <p>
+                <strong>Postal Code: </strong>
+                {cart.shippingAddress.postalCode}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
 
-export default PlaceOrderScreen
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {cart.cartItems.length > 0 ? (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => {
+                    return (
+                      <ListGroup.Item key={index}>
+                        <Row>
+                          <Col md={3}>
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              fluid
+                              rounded
+                            ></Image>
+                          </Col>
+                          <Col>
+                            <Link to={`/product/${item.product}`}>
+                              {item.name}
+                            </Link>
+                          </Col>
+                          <Col md={4}>
+                            {item.qty} x ${item.price} = $
+                            {Number((item.qty * item.price).toFixed(2))}
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    );
+                  })}
+                </ListGroup>
+              ) : (
+                <Message>Your cart is empty</Message>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="btn-block"
+                  disabled={cart.cartItems.length === 0}
+                  onClick={handlePlaceOrder}
+                >
+                  Order
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PlaceOrderScreen;
