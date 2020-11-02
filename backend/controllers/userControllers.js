ee487ffa1505ae134b302c718be3350f3c439826
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-02 14:07:41 +0200

    fixe double sending response

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -124,12 +124,11 @@ const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
   if (user) {
     await user.remove();
-    res.json({message:'User removed'})
+    res.json({ message: "User removed" });
   } else {
     res.status(404);
     throw new Error("User not found");
   }
-  res.json(users);
 });
 
 export {
