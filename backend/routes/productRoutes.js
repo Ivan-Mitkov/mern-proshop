2fee955af9411a33cbe1c6d4ad2a278db9757d4a
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-03 16:50:18 +0200

    create and update product backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,16 @@ import {
   getProductById,
   getProducts,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+router.post("/", protect, isAdmin, createProduct);
 router.delete("/:id", protect, isAdmin, deleteProduct);
+router.put("/:id", protect, isAdmin, updateProduct);
 
 export default router;
